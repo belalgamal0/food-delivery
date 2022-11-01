b990444c371287fcc0b682f9b1df0d99683295ad
BelAl <32741765+belalgamal0@users.noreply.github.com>
2022-11-02 01:44:37 +0200

    api provider
    Implement api provider service and utils to manage network requests and request metadata

---
+++ lib/core/api_provider/utils.dart
@@ -0,0 +1,12 @@
+class Utils {
+  static const String getFoodAuthority = "gunter-food-api.herokuapp.com";
+  static const String hiveBoxCache = "cache";
+  static const List<String> categories = [
+    "pizza",
+    "pasta",
+    "salads",
+    "drinks",
+    "sauces",
+    "sides"
+  ];
+}
