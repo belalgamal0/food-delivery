c87f973c59f219433b67e27f7cb50ff92ae897b9
BelAl <32741765+belalgamal0@users.noreply.github.com>
2022-11-02 02:32:43 +0200

    favorite feature data layer
    Implement favorite feature data layer and in includes local data source for caching (Hive), and also the implementation of the favorite repository contracts

---
+++ lib/features/favorite/data/datasources/favorite_local_datasource.dart
@@ -0,0 +1,44 @@
+import 'package:hive/hive.dart';
+
+abstract class FavoritesLocalDataSource {
+  Future<void> cacheFavorites(String item, String boxName);
+  Future<bool> isCached(String item, String boxName);
+}
+
+class FavoritesLocalDataSourceImpl implements FavoritesLocalDataSource {
+  final HiveInterface hive;
+
+  FavoritesLocalDataSourceImpl({required this.hive});
+
+  Future<Box<dynamic>> openHiveBox(String boxName) async {
+    final hiveBox = await Hive.openBox(boxName);
+    return hiveBox;
+  }
+
+  deleteCachedProduct(String item, String boxName) async {
+    final openBox = await openHiveBox(boxName);
+    openBox.delete(item);
+  }
+
+  @override
+  cacheFavorites(String item, String boxName) async {
+    final openBox = await openHiveBox(boxName);
+    bool cache = openBox.containsKey(item);
+    if (cache) {
+      await deleteCachedProduct(item, boxName);
+    } else {
+      await addItemToCache(item, boxName);
+    }
+  }
+
+  Future<void> addItemToCache(String item, String boxName) async {
+    final openBox = await openHiveBox(boxName);
+    openBox.put(item, true);
+  }
+
+  @override
+  isCached(String item, String boxName) async {
+    final openBox = await openHiveBox(boxName);
+    return openBox.containsKey(item);
+  }
+}
